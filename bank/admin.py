c3daef020973a0d4943f4086b99f3b6082c2339b
nacho <ignacio@digidis.lt>
2018-01-26 13:40:11 +0100

    added admin

--- bank/admin.py
+++ bank/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CompanyGroup,BankAccount
+from .models import CompanyGroup,BankAccount,Client
 
 admin.site.register(CompanyGroup)
 admin.site.register(BankAccount)
+admin.site.register(Client)
 # Register your models here.
