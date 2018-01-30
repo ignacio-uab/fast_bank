a49e2819b2e9a09bd107a8ed04acbd79cb0de766
nacho <ignacio@digidis.lt>
2018-01-30 18:15:25 +0100

    Eliminado Choose y Options de model

--- bank/models.py
+++ bank/models.py
@@ -41,7 +41,7 @@ class BankAccount (models.Model):
 class Client (models.Model):
     client_name = models.CharField(max_length=256)
     status_client = models.BooleanField (default=True)
-    owner = models.ForeignKey(CompanyGroup, on_delete=models.CASCADE,default="")
+#    owner = models.ForeignKey(CompanyGroup, on_delete=models.CASCADE,default="")
     def __str__(self):
         return self.client_name
 
