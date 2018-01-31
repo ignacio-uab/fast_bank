638b5846d705987193145a77994e0b4ce78c468e
nacho <ignacio@digidis.lt>
2018-01-31 16:19:24 +0100

    Added foreign key en el nombre

--- bank/models.py
+++ bank/models.py
@@ -36,7 +36,7 @@ class BankAccount (models.Model):
     company = models.ForeignKey(CompanyGroup, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.bic + " " + self.text
+        return self.company.company_name + " > " + self.bic + " " + self.text
 
 class Client (models.Model):
     client_name = models.CharField(max_length=256)
