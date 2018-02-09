47c07adcea42610cbe84d2ae82879dd40c2ff411
nacho <ignacio@digidis.lt>
2018-02-09 16:19:17 +0100

    django admin

--- bank/admin.py
+++ bank/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import CompanyGroup,BankAccount,Client,Movement,TypeofPayment
 
 # Register your models here.
+def make_checked(modeladmin, request, queryset):
+    queryset.update(checked='1')
+make_checked.short_description = "Mark selected movements as checked"
+
+class MovementAdmin(admin.ModelAdmin):
+    list_display = ['bank', 'amount','type','expiration_date']
+    ordering = ['expiration_date']
+    actions = [make_checked]
 
 class BankInLine (admin.TabularInline):
     model=BankAccount
@@ -15,6 +23,6 @@ admin.site.register(CompanyGroup,CompanyGroupAdmin)
 
 admin.site.register(Client)
 
-admin.site.register(Movement)
+admin.site.register(Movement,MovementAdmin)
 
 admin.site.register(TypeofPayment)
